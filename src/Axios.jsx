56d37d061a09c2285e4c156f5249d24de368331a
M M A ♔ <154054435+ahmadjon09@users.noreply.github.com>
2025-05-21 17:20:24 +0500

    Update Axios.jsx

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const is_auth = Cookies.get('client_token')
-// const BASE_URL = 'https://xt-server-w6zs.onrender.com'
-const BASE_URL = 'http://localhost:8000/'
+ const BASE_URL = 'https://xt-server-beta.onrender.com'
+// const BASE_URL = 'http://localhost:8000/'
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
